de42b8193e0c0fbd0845aa54fb0c15228f768202
Marcos Macedo <ricardo.paiva.macedo@hotmail.com>
2023-05-09 21:46:55 -0300

    Modelo de dominio

---
+++ src/main/java/com/example/demo/model/Belonging.java
@@ -0,0 +1,73 @@
+package com.example.demo.model;
+
+import jakarta.persistence.EmbeddedId;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_belonging")
+public class Belonging {
+    
+    @EmbeddedId
+    private BelongingPK id = new BelongingPK();
+    private Integer position;
+
+    public Belonging() {
+    }
+
+    public Belonging(Game game, GameList gameList, Integer position) {
+        id.setGame(game);
+        id.setGameList(gameList);
+        this.position = position;
+    }
+
+    public Game getGame(){
+        return id.getGame();
+    }
+
+    public GameList getGameList(){
+        return id.getGameList();
+    }
+
+    public void setGame(Game game){
+        id.setGame(game);
+    }
+
+    public void setGameList(GameList gameList){
+        id.setGameList(gameList);
+    }
+
+    public Integer getPosition() {
+        return position;
+    }
+
+    public void setPosition(Integer position) {
+        this.position = position;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Belonging other = (Belonging) obj;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        return true;
+    }
+
+}
