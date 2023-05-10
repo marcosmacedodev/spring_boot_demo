607e423a78bab6527710db37a88fad76b5ad9035
Marcos Macedo <ricardo.paiva.macedo@hotmail.com>
2023-05-09 22:40:17 -0300

    GET Game by Id

--- src/main/java/com/example/demo/controller/GameController.java
+++ src/main/java/com/example/demo/controller/GameController.java
@@ -4,9 +4,11 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.demo.model.dto.GameDto;
 import com.example.demo.model.dto.GameMinDto;
 import com.example.demo.services.GameService;
 
@@ -21,5 +23,8 @@ public class GameController {
     public List<GameMinDto> getAll(){
         return service.findAll();
     }
-    
+    @GetMapping("/{id}")
+    public GameDto get(@PathVariable Long id){
+        return service.findById(id);
+    }
 }
--- src/main/java/com/example/demo/services/GameService.java
+++ src/main/java/com/example/demo/services/GameService.java
@@ -1,12 +1,15 @@
 package com.example.demo.services;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.example.demo.model.Game;
+import com.example.demo.model.dto.GameDto;
 import com.example.demo.model.dto.GameMinDto;
 import com.example.demo.repositories.GameRepository;
 
@@ -16,9 +19,19 @@ public class GameService {
     @Autowired
     private GameRepository repository;
 
+    @Transactional(readOnly = true)
     public List<GameMinDto> findAll(){
         List<Game> list = repository.findAll();
         return list.stream().map((game) -> new GameMinDto(game)).collect(Collectors.toList());
+    }
+
+    @Transactional(readOnly = true)
+    public GameDto findById(Long id){
+
+        Optional<Game> game = repository.findById(id);
+
+        return new GameDto(game.orElse(null));
+
     }
     
 }
